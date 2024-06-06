f36e55b1f65bc053de4f3fe145c8e564e3a141c1
Ulf Luecking <ulf.luecking@gmail.com>
2024-06-07 00:20:08 +0200

    lint

--- Chapter07/audiofile/services/metadata/handler_search.go
+++ Chapter07/audiofile/services/metadata/handler_search.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -34,5 +35,8 @@ func (m *MetadataService) searchHandler(res http.ResponseWriter, req *http.Reque
 		res.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	io.WriteString(res, prettyJSON.String())
+	_, err = io.WriteString(res, prettyJSON.String())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
